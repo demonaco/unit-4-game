6a1e58508833e8a7e3a3b2a43499fb0a2172b5e1
Matt DeMonaco <mdemonaco@gmail.com>
2019-10-19 22:43:22 -0700

    added read.me section

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # unit-4-game
+
+## Summary: The purpose of this assignment was to get practical experience with jQuery, and to use some of the many methods that are included within the jQuery library. Overall I feel like I did better on this assignment than I did last week, but there were a couple of areas that tripped me up. While I can assign a random value to a click, I don't know how I can save that click and apply it's value each time the same crystal button is clicked. I also tried out code to add the random number of each crystal to an overall sum but it broke my code. Please see my psudo-coding for more on this and how the game should map out.
+
+## Steps:
+
+1. First, I wrote the HTML and tags for the elements
+2. I used CSS in a separate sheet to style the page. 
+3. I applied the jQuery CDN on the index.html page to link it to the game.js page. 
+4. I looked at the lotto generator excercise as a major reference for setting up random numbers. 
+5. I set each crystal button to spit out a randomly generated number from the array length + 1.
+6. I set up the wins and losses conditional statements to incremental either the losses or wins score.
+
+## Sources include: course drills, activites, fellow students.
+
+## See assets > images for screenshot of product.
--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,14 +1,3 @@
-
-// //set up array options for computer choice + user options
-// var scoreArray = [19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49,50,51,52,53,54,55,56,57,58,59,60,61,62,63,64,65,66,67,68,69,70,71,72,73,74,75,76,77,78,79,80,81,82,83,84,85,86,87,88,89,90,91,92,93,94,95,96,97,98,99,100,101,102,103,104,105,106,107,108,109,110,111,112,113,114,115,116,117,118,119,120]
-// var userOptions = [1,2,3,4,5,6,7,8,9,10,11,12] 
-
-// //set up our global variables
-// var wins = 0
-// var losses = 0
-// var computerNumber; 
-// var userScore = 0
-
 //let's get the window ready & have the number to add up to appear when the window is loaded
 $(window).ready(function computerValue() {
 
@@ -25,7 +14,7 @@ $(window).ready(function computerValue() {
 
 $("#crystal1").on("click", function () {
     var userScore1 = "";
-    var randomInt = Math.floor(Math.random() * userOptions.length);
+    var randomInt = Math.floor(Math.random() * userOptions.length + 1);
     console.log(randomInt);
     userScore1 = userScore1 + randomInt;
     
@@ -37,7 +26,7 @@ $("#crystal1").on("click", function () {
 
 $("#crystal2").on("click", function() {
     var userScore2 = "";
-    var randomInt = Math.floor(Math.random() * userOptions.length);
+    var randomInt = Math.floor(Math.random() * userOptions.length + 1);
     console.log(randomInt);
     userScore2 = userScore2 + randomInt;
 
@@ -49,7 +38,7 @@ $("#crystal2").on("click", function() {
 
 $("#crystal3").on("click", function () {
     var userScore3 = "";
-    var randomInt = Math.floor(Math.random() * userOptions.length);
+    var randomInt = Math.floor(Math.random() * userOptions.length + 1);
     console.log(randomInt);
     userScore3 = userScore3 + randomInt;
 
@@ -61,7 +50,7 @@ $("#crystal3").on("click", function () {
 
 $("#crystal4").on("click", function () {
     var userScore4 = "";
-    var randomInt = Math.floor(Math.random() * userOptions.length);
+    var randomInt = Math.floor(Math.random() * userOptions.length + 1);
     console.log(randomInt);
     userScore4 = userScore4 + randomInt;
 
@@ -74,79 +63,26 @@ $("#crystal4").on("click", function () {
 // Let's write an if statement that compares the total score to the computers score so that the wins or losses can be determined
 
 if ($("#setScore") === "computerNumber"){
-    wins++ }
+    wins.text++ }
 if ($("#setScore") > "computerNumber"){
-    losses++}
+    losses.text++}
 
-
+if (wins.text++ || losses.text++) {
+    computerNumber.clear();
+    userScore.clear();
+}
 });
 
 // 
 });
 
-// The .on method for click actions below should start a function that similiarly assigns a random number associated with the button and displays it on the #setScore div. 
-// We do this for each button
-
-// $("#crystal1").on("click", function() {
-//     var userScore = "";
-//     var randomInt = Math.floor(Math.random() * userOptions.length);
-//     console.log(randomInt);
-//     userScore = userScore + randomInt;
-
-
-//     $("#setScore").html(userScore);
-//     console.log(userScore);
-
-// });
-
-// $("#crystal2").on("click", function() {
-//     var userScore = "";
-//     var randomInt = Math.floor(Math.random() * userOptions.length);
-//     console.log(randomInt);
-//     userScore = userScore + randomInt;
-
-
-//     $("#setScore").text(userScore);
-//     console.log(userScore);
-
-// });
-
-// $("#crystal3").on("click", function () {
-//     var userScore = "";
-//     var randomInt = Math.floor(Math.random() * userOptions.length);
-//     console.log(randomInt);
-//     userScore = userScore + randomInt;
-
-
-//     $("#setScore").text(userScore);
-//     console.log(userScore);
-
-// });
-
-// $("#crystal4").on("click", function () {
-//     var userScore = "";
-//     var randomInt = Math.floor(Math.random() * userOptions.length);
-//     console.log(randomInt);
-//     userScore = userScore + randomInt;
-
-
-//     $("#setScore").text(userScore);
-//     console.log(userScore);
-
-// });
-
-//next we will add up the values of the buttons clicked
-
-
-
-
-//set up array options for computer choice + user options
+//set up global array options for computer choice + user options
 var scoreArray = [19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60, 61, 62, 63, 64, 65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 78, 79, 80, 81, 82, 83, 84, 85, 86, 87, 88, 89, 90, 91, 92, 93, 94, 95, 96, 97, 98, 99, 100, 101, 102, 103, 104, 105, 106, 107, 108, 109, 110, 111, 112, 113, 114, 115, 116, 117, 118, 119, 120]
 var userOptions = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
 
 //set up our global variables
-var wins = 0
-var losses = 0
+var wins = 0;
+var losses = 0;
 var computerNumber;
 var userScore;
 var randomInt;
